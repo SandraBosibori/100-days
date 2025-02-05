2bea7ce829c9d1b0e12334f051dc8fdb1e5bfa32
SandraBosibori <sandrahbosibori@gmail.com>
2025-02-05 15:32:21 +0300

    first calc

---
+++ day2/counter/calc.js
@@ -0,0 +1,27 @@
+let num = document.querySelector('.num')
+let count = 0
+let  c = document.querySelector('.c')
+let brackets = document.querySelector('.brackets')
+let modulo = document.querySelector('.modulo')
+let divide = document.querySelector('.divide')
+let multiply = document.querySelector('.multiply')
+let subtract = document.querySelector('.subtract')
+let add = document.querySelector('.add')
+let one = document.querySelector('.one')
+let two = document.querySelector('.two')
+let three = document.querySelector('.three')
+let four = document.querySelector('.four')
+let five = document.querySelector('.five')
+let six = document.querySelector('.six')
+let seven = document.querySelector('.seven')
+let eight = document.querySelector('.eight')
+let nine = document.querySelector('.nine')
+let zero = document.querySelector('.zero')
+let equal = document.querySelector('.equal')
+
+one.addEventListener('click', function(){
+    count = 1
+    num.textContent= count
+} 
+)
+
