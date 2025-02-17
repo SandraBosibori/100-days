e0611be683cbdef94707545c3f0de9f31622bafa
SandraBosibori <sandrahbosibori@gmail.com>
2025-02-17 23:20:08 +0300

    second number guess

--- day4/numguess.js
+++ day4/numguess.js
@@ -1,21 +1,45 @@
 let num = document.querySelector('.num')
 let low = document.querySelector('.low')
+let low2 = document.querySelector('.low2')
 
 const min = 1
 const max = 100
 
 const ans = Math.floor(Math.random() * (max - min + 1) + min)
 
+let tries = 3
+
 console.log(ans)
 
 num.addEventListener('change', function(){
     if (num.value > 100 || num.value < 1) {
         low.textContent = " Please enter a numer between 1 and 100"
-    }else if (num.value > 0 && num.value < 101 && num.value < ans){
+        tries --
+        low2.textContent = `You have ${tries} tries left`
+
+    }else{
+        tries --
+        
+    }
+
+    if (num.value > 0 && num.value < 101 && num.value < ans){
         low.textContent = "Good guess! Try a higher number"
+        if (tries>=0 && tries<=3){
+            low2.textContent = `You have ${tries} tries left`
+        }
     }else if (num.value > 0 && num.value < 101 && num.value > ans){
         low.textContent = "Good guess! Try a lower number"
+        if (tries>=0 && tries<=3){
+            low2.textContent = `You have ${tries} tries left`
+        }
     }else if (num.value == ans){
         low.textContent = "CORRECT"
+        if (tries>=0 && tries<=3){
+            low2.textContent = `You have ${tries} tries left`
+        }
+    }
+    if (tries==0){
+        low.textContent = "You have no more tries left"
+        num.disabled= true
     }
 })
